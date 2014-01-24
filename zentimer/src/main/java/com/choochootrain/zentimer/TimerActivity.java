08fd56bd9bc6eb644ff45252c685f3e4a42ee99f
Hurshal Patel <hpatel516@gmail.com>
2014-01-24 10:00:21 -0800

    add vibration on alarm trigger

--- zentimer/src/main/java/com/choochootrain/zentimer/TimerActivity.java
+++ zentimer/src/main/java/com/choochootrain/zentimer/TimerActivity.java
@@ -3,31 +3,56 @@ package com.choochootrain.zentimer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 
 public class TimerActivity extends Activity {
-    private running = false;
+    public static long MINUTES = 1000 * 60;
+
+    private boolean running = false;
+    private long startTime = 0;
+
+    private Vibrator vibrationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        vibrationManager = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
         setContentView(R.layout.activity_timer);
 
         final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
-        Button timerButton = (Button) findViewById(R.id.timer_button);
+        final Button timerButton = (Button) findViewById(R.id.timer_button);
 
         timerButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
                 running = !running;
-                if (!running)
-                  chronometer.start();
-                else
-                  chronometer.stop();
+                if (running) {
+                    startTime = SystemClock.elapsedRealtime();
+                    chronometer.setBase(startTime);
+                    chronometer.start();
+                    timerButton.setText(R.string.timer_button_stop);
+                } else {
+                    chronometer.stop();
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    timerButton.setText(R.string.timer_button_start);
+                }
+            }
+        });
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long currentTime = SystemClock.elapsedRealtime();
+                if (currentTime - startTime == 1 * MINUTES) {
+                    chronometer.stop();
+                    vibrationManager.vibrate(new long[] {300L, 200L, 300L, 200L, 1000L}, -1);
+                }
             }
         });
     }
