ee81d98939d33ca50785f3d3f1677b08e144e60e
Hurshal Patel <hpatel516@gmail.com>
2014-01-25 18:35:33 -0800

    refactor chronometer start and stop

--- zentimer/src/main/java/com/choochootrain/zentimer/TimerActivity.java
+++ zentimer/src/main/java/com/choochootrain/zentimer/TimerActivity.java
@@ -15,6 +15,9 @@ public class TimerActivity extends Activity {
 
     private Vibrator vibrator;
 
+    private Chronometer chronometer;
+    private Button timerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +26,17 @@ public class TimerActivity extends Activity {
 
         setContentView(R.layout.activity_timer);
 
-        final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
-        final Button timerButton = (Button) findViewById(R.id.timer_button);
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        timerButton = (Button) findViewById(R.id.timer_button);
 
         timerButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                if (!running) {
-                    chronometer.setBase(SystemClock.elapsedRealtime());
-                    chronometer.start();
-                    timerButton.setText(R.string.timer_button_stop);
-                } else {
-                    chronometer.stop();
-                    chronometer.setBase(SystemClock.elapsedRealtime());
-                    timerButton.setText(R.string.timer_button_start);
-                }
+                if (!running)
+                    startTimer();
+                else
+                    stopTimer();
 
                 running = !running;
             }
@@ -51,6 +49,7 @@ public class TimerActivity extends Activity {
                 if (elapsedTime >= 1 * MINUTES) {
                     chronometer.stop();
                     vibrator.vibrate(new long[] {300L, 200L, 300L, 200L, 1000L}, -1);
+                    stopTimer();
                 }
             }
         });
@@ -59,5 +58,17 @@ public class TimerActivity extends Activity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+    }
+
+    private void startTimer() {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.start();
+        timerButton.setText(R.string.timer_button_stop);
+    }
+
+    private void stopTimer() {
+        chronometer.stop();
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        timerButton.setText(R.string.timer_button_start);
     }
 }
